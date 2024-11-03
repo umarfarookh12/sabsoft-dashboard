553f6598593c250e940feffe0fb5e35baece0d61
umar <sdumarfarookh212@gmail.com>
2024-11-03 22:37:22 +0530

    used Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./dashboard/Home";
 
 function App() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
